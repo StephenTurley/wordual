08b6edef6487c6bad905da8af9cc39c3372daf78
Stephen Turley <stephen@turley.dev>
2023-12-16 10:37:01 -0500

    removed repo check from endpoint since we don't have a db

--- lib/wordual_web/endpoint.ex
+++ lib/wordual_web/endpoint.ex
@@ -28,7 +28,6 @@ defmodule WordualWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :wordual
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
